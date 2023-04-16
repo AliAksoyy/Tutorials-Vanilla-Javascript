a2bc2f75f464bc2c3bb2dc75f624ebea6dd46c7d
AliAksoyy <alifeyza4017@gmail.com>
2023-04-16 12:48:52 +0300

    validation contunie

--- Email-Validation/main.js
+++ Email-Validation/main.js
@@ -1,17 +1,29 @@
-function validation() {
-  let form = document.getElementById("form");
-  let email = document.getElementById("email");
-  let text = document.getElementById("text");
-  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-console.log(email.value.match(pattern));
 
-  if (email.value.match(pattern)) {
-    console.log(email.match(pattern));
+
+email.addEventListener("keydown", () => {
+
+let form = document.getElementById("form");
+let email = document.getElementById("email").value;
+let text = document.getElementById("text");
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+
+
+  if (email.match(pattern)) {
     form.classList.add("valid");
     form.classList.remove("invalid");
+    text.innerHTML="Your Email Address inValid"
+    text.style.color="#00ff00"
   } else {
-    form.classList.add("invalid");
     form.classList.remove("valid");
+    form.classList.add("invalid");
+     text.innerHTML = "Your Email Address Valid";
+     text.style.color = "#ff0000";
   }
-}
-validation()
+  if(email.value === ""){
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+     text.innerHTML = "";
+     text.style.color = "#ff0000";
+  }
+});
